0c34fdbde2f3ba690851f115306987f97fcb9011
Mahmut <Mahmut@DESKTOP-G9AUQ7V>
2022-05-17 14:24:31 +0300

    Firma,Firma Calisanlari,Etkinlik Kayitlari,Etkinlik Ayrinti Tabloları kaldı.

---
+++ Entities/Concrete/Islemler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entities.Concrete
+{
+    public class Islemler
+    {
+        public int islem_id { get; set; }
+        public string islem_adi { get; set; }
+        public int unvan_id { get; set; }
+    }
+}
---
+++ Entities/Concrete/Randevu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Entities.Concrete
+{
+    public class Randevu
+    {
+        public int randevu_id { get; set; }
+        public DateTime randevu_tarihi { get; set; }
+        public int firma_id { get; set; }
+        public bool randevu_durumu { get; set; }
+        public string aciklama { get; set; }
+    }
+}
